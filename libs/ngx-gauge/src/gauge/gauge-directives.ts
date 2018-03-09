2b3fe8cd80cbf462827eb9fd6a88f2e355470f09
Ashish Chopra <sendtoashishchopra@gmail.com>
2018-03-09 09:13:39 +0530

    gauge-directives added

---
+++ libs/ngx-gauge/src/gauge/gauge-directives.ts
@@ -0,0 +1,21 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+  selector: "ngx-gauge-append"
+})
+export class NgxGaugeAppend {}
+
+@Directive({
+  selector: "ngx-gauge-prepend"
+})
+export class NgxGaugePrepend {}
+
+@Directive({
+  selector: "ngx-gauge-value"
+})
+export class NgxGaugeValue {}
+
+@Directive({
+  selector: "ngx-gauge-label"
+})
+export class NgxGaugeLabel {}
