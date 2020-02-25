11d61235be3856d49f12456722ad850db975bb36
Rumit Parakhiya <2221855+rparakhiya@users.noreply.github.com>
2020-02-25 16:04:45 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxGaugeModule } from 'projects/ngx-gauge/src/ngx-gauge.module';
+import { NgxGaugeModule } from 'ngx-gauge';
 
 @NgModule({
   declarations: [
