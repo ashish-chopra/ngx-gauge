01861477a1cc591d987463576f4cb0b5ba43078a
Daniel Santiago <daniel@shipshape.co.za>
2021-06-18 10:22:59 +0200

    * Migrate to prod build by default * Export required component/directives

--- projects/ngx-gauge/src/public_api.ts
+++ projects/ngx-gauge/src/public_api.ts
@@ -3,3 +3,5 @@
  */
 
 export * from './ngx-gauge.module';
+export { NgxGauge } from './gauge/gauge';
+export { NgxGaugeAppend, NgxGaugeLabel, NgxGaugePrepend, NgxGaugeValue } from './gauge/gauge-directives';
