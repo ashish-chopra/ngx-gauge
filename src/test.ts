a303a24dc58003a799ae4ebed13518d0b4446af6
Daniel Santiago <daniel@shipshape.co.za>
2022-03-25 12:19:46 +0200

    revert formatting change

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import "zone.js/testing";
-import { getTestBed } from "@angular/core/testing";
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
-} from "@angular/platform-browser-dynamic/testing";
+} from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
 
@@ -16,6 +16,6 @@ getTestBed().initTestEnvironment(
   { teardown: { destroyAfterEach: false } }
 );
 // Then we find all the tests.
-const context = require.context("./", true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
