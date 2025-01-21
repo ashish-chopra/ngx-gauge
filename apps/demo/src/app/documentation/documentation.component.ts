9dbc085f994176ae0919cc3f19893585367fa138
Addison Emig <addison.emig@mrs-electronics.com>
2025-01-21 09:09:46 -0500

    chore: upgrade to angular 19

--- apps/demo/src/app/documentation/documentation.component.ts
+++ apps/demo/src/app/documentation/documentation.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-documentation',
-  templateUrl: './documentation.component.html',
-  styleUrls: ['./documentation.component.css']
+    selector: 'app-documentation',
+    templateUrl: './documentation.component.html',
+    styleUrls: ['./documentation.component.css'],
+    standalone: false
 })
 export class DocumentationComponent implements OnInit {
 
--- projects/ngx-gauge/src/gauge/gauge-directives.ts
+++ projects/ngx-gauge/src/gauge/gauge-directives.ts
@@ -1,25 +1,29 @@
 import { Directive } from "@angular/core";
 
 @Directive({
-  selector: "ngx-gauge-append",
-  exportAs: "ngxGaugeAppend"
+    selector: "ngx-gauge-append",
+    exportAs: "ngxGaugeAppend",
+    standalone: false
 })
 export class NgxGaugeAppend {}
 
 @Directive({
-  selector: "ngx-gauge-prepend",
-  exportAs: "ngxGaugePrepend"
+    selector: "ngx-gauge-prepend",
+    exportAs: "ngxGaugePrepend",
+    standalone: false
 })
 export class NgxGaugePrepend {}
 
 @Directive({
-  selector: "ngx-gauge-value",
-  exportAs: "ngxGaugeValue"
+    selector: "ngx-gauge-value",
+    exportAs: "ngxGaugeValue",
+    standalone: false
 })
 export class NgxGaugeValue {}
 
 @Directive({
-  selector: "ngx-gauge-label",
-  exportAs: "ngxGaugeLabel"
+    selector: "ngx-gauge-label",
+    exportAs: "ngxGaugeLabel",
+    standalone: false
 })
 export class NgxGaugeLabel {}
